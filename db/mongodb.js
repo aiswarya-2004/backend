40ebed08ab152e7e0aa5fb227b604a63fbe0b772
Aiswarya us <Aiswaryaus20@gmail.com>
2024-10-29 15:00:02 +0530

    hgghghfg

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect("mongodb+srv://sasid717:lUd6mQ5ywQbUR9KV@cluster0.royx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+module.exports = mongoose.connect("mongodb+srv://sasid717:lUd6mQ5ywQbUR9KV@cluster0.royx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/backend")
     .then((result) => {
         console.log("");
     })
