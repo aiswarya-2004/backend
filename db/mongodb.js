b9cd307a2181bb15f211723e90998716106789c5
Aiswarya us <Aiswaryaus20@gmail.com>
2024-10-29 14:48:47 +0530

    hello

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect("mongodb://localhost:27017")
+module.exports = mongoose.connect("mongodb+srv://sasid717:lUd6mQ5ywQbUR9KV@cluster0.royx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then((result) => {
         console.log("");
     })
