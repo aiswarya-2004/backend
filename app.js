6e3faf994f6305021e2e02a7640c8e4b6302bc1f
Aiswarya us <Aiswaryaus20@gmail.com>
2024-11-01 11:47:20 +0530

    right

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
